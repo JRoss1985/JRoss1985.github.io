ee6f384bbc1cc9c37175230e03de628690db16ff
JRoss1985 <153573757+JRoss1985@users.noreply.github.com>
2023-12-14 11:59:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Code to run after the DOM is loaded
+    /*
+    const str = "A string"; // String
+    const num = 3; // Number
+    const flt = 3.14; // Float
+    const bool = true; // or "false"
+    const list = ["Item 1", 2, "Three", 3.14, []]; // Array = list
+    const thing = null; // has nothing
+    const notDefined = undefined; // Currently holds no value
+    const getAge = function(){return 31;} // this is a function
+    const obj = {
+        'name':'Kalob',
+        'profession':'Coding Teacher'
+    } // Object; can be accessed with obj['name']
+*/
+
+
+    // Example: Change the content of an element with the ID 'myElement'
+    var myElement = document.getElementById('myElement');
+    myElement.innerHTML = 'Hello, World!';
+});
